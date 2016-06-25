2fdd00e49d4e1e3ca30144babc9e04c0d23decf0
Easelm <tommy@nomsoft.net>
2016-06-25 03:10:08 -0400

    Tutorial2

---
+++ Tutorial2/Tutorial2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tutorial2
+{
+    public partial class Form1 : Form
+    {
+        public string testString = "Hello, you called me from form2.";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Form2(this).Show();
+        }
+    }
+}
---
+++ Tutorial2/Tutorial2/Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tutorial2
+{
+    public partial class Form2 : Form
+    {
+        private Form1 form1;
+
+        public Form2(Form1 f)
+        {
+            InitializeComponent();
+
+            form1 = f;
+            textBox1.Text = form1.testString;
+        }
+    }
+}
