6770bccc91226b79a46251b2e19aea1f3a384e12
Easelm <tommy@nomsoft.net>
2016-06-30 06:29:30 -0400

    New tutorials

---
+++ Tutorials/C# Unity Tutorials - Unity Tutorials/UnityTutorial2/Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D playerRigidbody2D;
+
+    private float jumpForce;
+    private bool isJumping;
+
+    // Use this for initialization
+    void Start()
+    {
+        jumpForce = 100;
+        isJumping = false;
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            playerRigidbody2D.AddForce(new Vector2(0, jumpForce));
+            isJumping = true;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Ground")
+            isJumping = false;
+    }
+}
